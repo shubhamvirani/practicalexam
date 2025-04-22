faf3822f2cfa464c8178f2a5fce34dc608688ba5
shubhamvirani <137860991+shubhamvirani@users.noreply.github.com>
2025-04-22 15:39:11 +0530

    Update version1.py

--- version1.py
+++ version1.py
@@ -1,16 +1,19 @@
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    if b!=0:
+        return a/b
+    else:
+        print("divison cannot be calculated")
+
 num1=float(input("enter number 1:"))
 num2=float(input("enter number 2:"))
 
-add=num1+num2
-sub=num1-num2
-mul=num1*num2
-
-if num2!=0:
-    div=num1/num2
-else:
-    print("div cannot be calculated")
-
-print(f"addition:{add}")
-print(f"subtraction:{sub}")
-print(f"multiplication:{mul}")
-print(f"division:{div}")
+print(f"addition:{add(num1,num2)}")
+print(f"subtraction:{sub(num1,num2)}")
+print(f"multiplication:{mul(num1,num2)}")
+print(f"division:{div(num1,num2)}")
